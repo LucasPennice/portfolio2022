69da51d8cf0e669c1c4c852cc9cceda3aeeb4d7f
Lucas Pennice <lucaspennice@gmail.com>
2022-11-16 17:06:45 -0300

    remove threejs lol

--- components/ContentSection/HeroSection/index.tsx
+++ components/ContentSection/HeroSection/index.tsx
@@ -1,14 +1,19 @@
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
+import useIsMobile from "../../../utils/useMobileScreen";
+import AnimateWordOnView from "../../AnimateWordOnView";
 const Laptop = dynamic(() => import("./Laptop"), { ssr: false });
 
 function HeroSection() {
+    const isMobile = useIsMobile(500);
     return (
         <div style={{ maxWidth: 2500, margin: "0 auto" }} className="w-full flex-col justify-start items-center">
-            <div className={`w-full flex-col justify-center align-center relative`} style={{ height: "100vh" }}>
-                <Suspense fallback={Loading()}>
+            <div className={`w-full flex flex-col justify-center items-center relative`} style={{ height: "100vh" }}>
+                <AnimateWordOnView fontSize={isMobile ? 70 : 110} wordToAnimate="LUCAS" />
+                <AnimateWordOnView fontSize={isMobile ? 70 : 110} wordToAnimate="PENNICE" />
+                {/* <Suspense fallback={Loading()}>
                     <Laptop />
-                </Suspense>
+                </Suspense> */}
             </div>
         </div>
     );
